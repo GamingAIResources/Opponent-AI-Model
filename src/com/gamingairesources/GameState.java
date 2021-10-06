b48787b0460ebc7f4763428e4c88ee3fb11e6943
Paul Biocco <pjbiocco@gmail.com>
2021-10-06 00:13:29 -0400

    Merging GameState with GameStateNode since functionatity functioned better there.

--- src/com/gamingairesources/GameState.java
+++ src/com/gamingairesources/GameState.java
@@ -1,5 +1,6 @@
 
 package com.gamingairesources;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public abstract class GameState implements Comparable<GameState>{
@@ -11,7 +12,8 @@ public abstract class GameState implements Comparable<GameState>{
 
 
     public abstract int evaluateState();
-    public abstract ArrayList<GameState>  generatePossibleMoves();
+    public abstract ArrayList<GameState> generatePossibleMoves();
+    public abstract int interpretNoMoves();
 
     public GameState(){                         //Used for testing?
         setPlayerTurn(1);
@@ -23,6 +25,31 @@ public abstract class GameState implements Comparable<GameState>{
         this.setPlayerTurn(playerTurn);
         setStateEvaluation(evaluateState());
         setEvaluated(true);
+    }
+
+    public int searchMove(GameState currentState, int depth, int alpha, int beta){
+        if(depth == 0){
+            return evaluateState();
+        }
+
+        ArrayList<GameState> childrenMoves = new ArrayList<>();
+        if(childrenMoves.size() == 0){
+            return interpretNoMoves();
+        }
+
+        for(GameState childMove : childrenMoves){
+            int evaluation = searchMove(childMove,depth-1, -1*beta, -1*alpha);
+
+            //Move is too strong, so AI should make another move.
+            if(evaluation >= beta){
+                return beta;
+            }
+
+            alpha = Math.max(alpha, evaluation);
+
+        }
+        
+        return alpha;
     }
 
 
